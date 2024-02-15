d5d785c85b3447b8cd3c6af8be647be2df90858b
savelievamareena <savelieva.mareena.1.21@gmail.com>
2024-02-15 16:00:54 +0300

    refactor: format tests

--- tests/Operation.test.js
+++ tests/Operation.test.js
@@ -1,40 +1,40 @@
-import {beforeEach, describe, expect, test} from '@jest/globals';
+import { beforeEach, describe, expect, test } from "@jest/globals";
 import OperationTwoOperands from "../src/classes/OperationTwoOperands";
 
-describe('Operation', () => {
+describe("OperationTwoOperands", () => {
     let operation;
 
     beforeEach(() => {
         operation = new OperationTwoOperands();
     });
 
-    test('adds two numbers', () => {
-        expect(operation.execute('add', 1, 2)).toBe(3);
-        expect(operation.execute('add', -5, 0)).toBe(-5);
+    test("adds two numbers", () => {
+        expect(operation.execute("add", 1, 2)).toBe(3);
+        expect(operation.execute("add", -5, 0)).toBe(-5);
     });
 
-    test('subtracts two numbers', () => {
-        expect(operation.execute('subtract', 5, 3)).toBe(2);
-        expect(operation.execute('subtract', 100, -5)).toBe(105);
+    test("subtracts two numbers", () => {
+        expect(operation.execute("subtract", 5, 3)).toBe(2);
+        expect(operation.execute("subtract", 100, -5)).toBe(105);
     });
 
-    test('multiplies two numbers', () => {
-        expect(operation.execute('multiply', 4, 5)).toBe(20);
-        expect(operation.execute('multiply', 100, 0)).toBe(0);
+    test("multiplies two numbers", () => {
+        expect(operation.execute("multiply", 4, 5)).toBe(20);
+        expect(operation.execute("multiply", 100, 0)).toBe(0);
     });
 
-    test('divides two numbers', () => {
-        expect(operation.execute('divide', 10, 2)).toBe(5);
-        expect(operation.execute('divide', 10, 0)).toBe('Error');
+    test("divides two numbers", () => {
+        expect(operation.execute("divide", 10, 2)).toBe(5);
+        expect(operation.execute("divide", 10, 0)).toBe("Error");
     });
 
-    test('calculates the y root of x', () => {
-        expect(operation.execute('rootY', 27, 3)).toBeCloseTo(3);
-        expect(operation.execute('rootY', 16, 0)).toBe('Error');
+    test("calculates the y root of x", () => {
+        expect(operation.execute("rootY", 27, 3)).toBeCloseTo(3);
+        expect(operation.execute("rootY", 16, 0)).toBe("Error");
     });
 
-    test('calculates x raised to the power of y', () => {
-        expect(operation.execute('exponentiationY', 2, 3)).toBe(8);
-        expect(operation.execute('exponentiationY', 1, 0)).toBe(1);
+    test("calculates x raised to the power of y", () => {
+        expect(operation.execute("exponentiationY", 2, 3)).toBe(8);
+        expect(operation.execute("exponentiationY", 1, 0)).toBe(1);
     });
 });
